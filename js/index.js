1ff4de461a5b2fb9f744a27e029cb8846d6bcfc5
409 <409@academicos.cesde.edu.co>
2023-02-24 20:15:36 -0500

    formulario

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let boton = document.createElement('button')
+let Cotizador = document.querySelector('#Cotizador')
+boton.setAttribute('class','boton')
+boton.textContent="calcular"
+
+Cotizador.appendChild(boton)
